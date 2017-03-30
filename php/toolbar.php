03280c67e303d23c3c3d4a4183b606fb074c5195
DiscordCollab <cidu@vps30.com>
2017-03-30 08:18:09 -0700

    Create toolbar.php

---
+++ php/toolbar.php
@@ -0,0 +1,25 @@
+<header id="header" class="wrapper">
+    <div id="header-inner" class="inner">
+        <h1 id="logo-wrap"> 
+          <a href="https://betterdocs.net/" id="logo">BetterDiscord</a> 
+      </h1>
+        <nav id="main-nav">
+            <a href="https://betterdocs.net" class="main-nav-link">Docs</a>
+            <a href="https://betterdocs.net/news/" class="main-nav-link">News</a>
+            <a href="https://betterdocs.net/plugin_api.html" class="main-nav-link">API</a>
+            <a href="https://betterdocs.net/themes.html" class="main-nav-link">Themes</a>
+            <a href="https://betterdocs.net/plugins.html" class="main-nav-link">Plugins</a>
+            <a href="https://github.com/0mniscient/" class="main-nav-link"><i class="fa fa-github-alt"></i></a> </nav>
+        <div id="lang-select-wrap">
+            <label id="lang-select-label"><i class="fa fa-globe"></i><span>English</span></label>
+            <select id="lang-select" data-canonical="docs/index.html">
+                <option value="en" selected="">English</option>
+            </select>
+        </div>
+        <a id="mobile-nav-toggle">
+            <span class="mobile-nav-toggle-bar"></span>
+            <span class="mobile-nav-toggle-bar"></span>
+            <span class="mobile-nav-toggle-bar"></span> </a>
+    </div>
+
+</header>
